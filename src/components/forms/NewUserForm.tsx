df280293d0e61f79c3abcb2f177db962f54c7ae8
eyram <pistisray@gmail.com>
2024-08-17 15:02:59 +0000

    signup form cleanup

--- src/components/forms/NewUserForm.tsx
+++ src/components/forms/NewUserForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react'
+import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '../ui/form';
 import { Input } from '../ui/input';
@@ -8,6 +8,7 @@ import { Button } from '../ui/button';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../ui/select';
+import { Eye, EyeOff } from 'lucide-react';
 import toast from 'react-hot-toast';
 
 
@@ -18,7 +19,9 @@ const formSchema = z.object({
         description:'Email of user',
         required_error: 'Please enter a valid email address'
     }).email(),
-    user_type: z.enum(USER_TYPES),
+    user_type: z.enum(USER_TYPES, {
+  errorMap: (issue, ctx) => ({ message: 'Please select a user type' })
+}),
     password: z.string({
         description:'Password of user',
         required_error: 'Please enter a password',
@@ -30,34 +33,36 @@ const formSchema = z.object({
 });
 
 const NewUserForm = () => {
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] =useState(false);
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
     });
+     const {
+         formState: { isDirty, isSubmitting },
+  } = form;
 
-    // function onSubmit(values: z.infer<typeof formSchema>) {
-    //     // if(values.password.toLowerCase() !== values.confirm_password.toLowerCase()) {
-    //     //     return toast.error('Passwords do not match');
-    //     // }
-    //     // Do something with the form values.
-    //     console.log(values)
-    // }
-    const onSubmit: SubmitHandler<z.infer<typeof formSchema>> = async(data) =>{
-     if(data.password.toLowerCase() !== data.confirm_password.toLowerCase()) {
-              return toast.error('Passwords do not match');
-          }
-        //   Do something with the form data.
-         console.log(data)
+    const onSubmit: SubmitHandler<z.infer<typeof formSchema>> = async (data) =>{
+        try{
+            if(data.password.toLowerCase() !== data.confirm_password.toLowerCase()) {
+                     return toast.error('Passwords do not match');
+                 }
+               //   Do something with the form data.
+                console.log(data)
+        }catch(error){
+            console.error(error);
+        }
     }
     return (
         <div className='w-full'>
             <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
                     <FormField
                         control={form.control}
                         name="email"
                         render={({ field }) => (
                             <FormItem>
-                                <FormLabel>email</FormLabel>
+                                <FormLabel className='text-lg font-semibold'>Email</FormLabel>
                                 <FormControl>
                                     <Input type='email' placeholder="example@eg.com" {...field} />
                                 </FormControl>
@@ -70,9 +75,26 @@ const NewUserForm = () => {
                         name="password"
                         render={({ field }) => (
                             <FormItem>
-                                <FormLabel>Password</FormLabel>
+                                <FormLabel className='text-lg font-semibold'>Password</FormLabel>
                                 <FormControl>
-                                    <Input type='password' placeholder="Enter password" {...field} />
+                                    <div className="relative">
+                                        <Input
+                                            type={showPassword ? 'text' : 'password'}
+                                            placeholder="Enter password"
+                                            {...field}
+                                        />
+                                        <button
+                                            type="button"
+                                            className="absolute inset-y-0 right-0 pr-3 flex items-center"
+                                            onClick={() => setShowPassword(!showPassword)}
+                                        >
+                                            {showPassword ? (
+                                                <EyeOff className="h-5 w-5 text-gray-400" />
+                                            ) : (
+                                                <Eye className="h-5 w-5 text-gray-400" />
+                                            )}
+                                        </button>
+                                    </div>
                                 </FormControl>
                                 <FormMessage />
                             </FormItem>
@@ -83,9 +105,26 @@ const NewUserForm = () => {
                         name="confirm_password"
                         render={({ field }) => (
                             <FormItem>
-                                <FormLabel>Confirm Password</FormLabel>
+                                <FormLabel className='text-lg font-semibold'>Confirm Password</FormLabel>
                                 <FormControl>
-                                    <Input type='password' placeholder="Confirm password" {...field} />
+                                    <div className="relative">
+                                        <Input
+                                            type={showConfirmPassword ? 'text' : 'password'}
+                                            placeholder="Confirm password"
+                                            {...field}
+                                        />
+                                        <button
+                                            type="button"
+                                            className="absolute inset-y-0 right-0 pr-3 flex items-center"
+                                            onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                                        >
+                                            {showConfirmPassword ? (
+                                                <EyeOff className="h-5 w-5 text-gray-400" />
+                                            ) : (
+                                                <Eye className="h-5 w-5 text-gray-400" />
+                                            )}
+                                        </button>
+                                    </div>
                                 </FormControl>
                                 <FormMessage />
                             </FormItem>
@@ -96,7 +135,7 @@ const NewUserForm = () => {
                         name="user_type"
                         render={({ field }) => (
                             <FormItem>
-                                <FormLabel>User Type</FormLabel>
+                                <FormLabel className='text-lg font-semibold'>User Type</FormLabel>
                                 <Select onValueChange={field.onChange} defaultValue={field.value}>
                                     <FormControl>
                                         <SelectTrigger>
@@ -115,7 +154,7 @@ const NewUserForm = () => {
                             </FormItem>
                         )}
                     />
-                    <Button type="submit" className='w-full'>Register</Button>
+                    <Button type="submit" disabled={!isDirty || isSubmitting} className='w-full disabled:disabled:pointer-events-auto disabled:bg-opacity-70'>{isSubmitting? 'Submitting': 'Register'}</Button>
                 </form>
             </Form>
         </div>
