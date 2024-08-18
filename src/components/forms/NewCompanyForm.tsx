ae6b1b281e4b5d81ecac5df7c81a2749601b26e8
eyram <pistisray@gmail.com>
2024-08-18 23:42:15 +0000

    update

--- src/components/forms/NewCompanyForm.tsx
+++ src/components/forms/NewCompanyForm.tsx
@@ -1,20 +1,27 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import React from 'react'
+import { useForm } from 'react-hook-form';
 import { z } from 'zod'
 
+const companySchema = z.object({
+    name: z.string({
+        description: 'Company name',
+        required_error: 'Please enter a company name',
+    }),
+    description: z.string({
+        description: 'Company description',
+        required_error: 'Please provide a description of your company',
+    }),
+    logo_url: z.string(),
+    website_url: z.string().url(),
+    email: z.string().email(),
+})
+type TCompanySchema = z.infer<typeof companySchema>;
 const NewCompanyForm = () => {
-    const companySchema = z.object({
-        name: z.string({
-            description: 'Company name',
-            required_error: 'Please enter a company name',
-        }),
-        description: z.string({
-            description: 'Company description',
-            required_error: 'Please provide a description of your company',
-        }),
-        logo_url: z.string(),
-        website_url: z.string().url(),
-        email: z.string().email(),
-    })
+    const form = useForm<TCompanySchema>({
+        resolver: zodResolver(companySchema),
+    });    
+    
   return (
     <div>NewCompanyForm</div>
   )
