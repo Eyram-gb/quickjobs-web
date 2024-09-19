779ffe76167041fcd1dd0166fbcec7bff921c6d2
eyram <pistisray@gmail.com>
2024-09-19 21:10:41 +0000

    fetch employer applications

--- src/components/ApplicationsTable.tsx
+++ src/components/ApplicationsTable.tsx
@@ -1,8 +1,21 @@
+'use client'
+import { useAuthStore } from "@/lib/store/authStore"
 import { columns, Application } from "./tables/columns"
 import { DataTable } from "./tables/DataTable"
+import { useQuery } from '@tanstack/react-query'
+import { API_BASE_URL } from "@/lib/constants"
 
 export default function ApplicationsTable() {
-  const data: Application[] = [
+  const { employer_profile } = useAuthStore()
+const {isPending, isError, data, error} = useQuery<Application[]>({
+  queryKey: ['employer_applications'], queryFn: () =>
+            fetch(`${API_BASE_URL}/applications/employer/${employer_profile?.id}`).then(
+                (res) => res.json()
+            )
+})
+ if (isPending) return <div>Loading...</div>
+    if (isError) return <div>Error: {error.message}</div>
+  const dataT: Application[] = [
     {
       application_id: "c85ee288-b5fa-49b2-88f4-5c7666a49d18",
       gig_id: "0ba5f56a-807a-48b4-9f10-c1be3050bfb3",
--- src/components/EmployerGigs.tsx
+++ src/components/EmployerGigs.tsx
@@ -15,7 +15,6 @@ const EmployerGigs = () => {
                 (res) => res.json()
             )
     })
-    console.log(data)
 
     if (isPending) return <div>Loading...</div>
     if (isError) return <div>Error: {error.message}</div>
